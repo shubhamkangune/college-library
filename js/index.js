9f5c9f56c9342f4d96048b9613308141224978ef
Shubham Kangune <shubhamcsc4656@gmail.com>
2025-02-16 15:29:52 +0530

    submit button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log("this is index.js")
+console.log("this is index.js");
+
+// Constructors
+
+function Book(name,author,type){
+
+}
+
+// display constructors
+
+function Display(){
+
+}
+
+// Add methods to display prototypes
+
+
+
+
+// add submit event listener to libraryForm
+
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e){
+    e.preventDefault();
+    console.log("you have submited the library form");
+}
